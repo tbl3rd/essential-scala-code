0014d7fcc2c905987b8f9eb086eba8dc3ac1b01e
Andrea Haessly <ahaessly@broadinstitute.org>
2017-06-07 10:33:02 -0400

    flatMap impl

--- src/main/scala/adt/intlist/MyList.scala
+++ src/main/scala/adt/intlist/MyList.scala
@@ -4,42 +4,61 @@ sealed trait MyOption[A]
 
 case class MySome[A](value: A) extends MyOption[A]
 
-case class MyNone[A]() extends MyOption[A]
+case class MyNone[Nothing]() extends MyOption[Nothing]
 
-sealed trait MyList[A] {
+sealed trait MyList[+A] {
 
-  def contains(i: A): Boolean = this match {
-    case MyListNil() => false
+  def contains[AA >: A](i: AA): Boolean = this match {
+    case MyListNil => false
     case MyListPair(head, _) if head == i => true
     case MyListPair(_, tail) => tail contains i
   }
 
 
   def exists(f: A => Boolean): Boolean = this match {
-    case MyListNil() => false
+    case MyListNil => false
     case MyListPair(head, tail) => f(head) || tail.exists(f)
   }
 
   def filter(f: A => Boolean): MyList[A] = this match {
-    case MyListNil() => this
+    case MyListNil => this
     case MyListPair(head, tail) if f(head) => MyListPair(head, tail.filter(f))
     case MyListPair(_, tail) => tail.filter(f)
   }
 
-  def find(f: A => Boolean): MyOption[A] = this match {
-    case MyListNil() => MyNone[A]()
+  def find[AA >: A](f: AA => Boolean): MyOption[AA] = this match {
+    case MyListNil => MyNone[AA]()
     case MyListPair(head, _) if f(head) => MySome(head)
     case MyListPair(_, tail) => tail.find(f)
   }
+
+  // AA is supertype of A
+  def append[AA >: A](that: MyList[AA]): MyList[AA] = this match {
+    case MyListNil => that
+    case MyListPair(head, tail) => MyListPair(head, tail.append(that))
+  }
+
+  def map[B](f: A => B): MyList[B] = this match {
+    case MyListNil => MyListNil
+    case MyListPair(head, tail) => MyListPair(f(head), tail.map(f))
+  }
+
+  def flatMap[B](f: A => MyList[B]): MyList[B] = this match {
+    case MyListNil => MyListNil
+    case MyListPair(head, tail) =>
+      val x: MyList[B] = f(head)
+      val y: MyList[B] = tail.flatMap(f)
+      x.append(y)
+  }
+
 }
 
-case class MyListNil[A]() extends MyList[A]
+case object MyListNil extends MyList[Nothing]
 
 case class MyListPair[A](head: A, tail: MyList[A]) extends MyList[A]
 
 object Main extends App {
-
-  val ints = MyListPair(1, MyListPair(2, MyListPair(3, MyListNil())))
+  val ints = MyListPair(1, MyListPair(2, MyListPair(3, MyListNil)))
 
   println(ints + """.contains(1) == """ + ints.contains(1))
   println(ints + """.contains(5) == """ + ints.contains(5))
@@ -56,4 +75,9 @@ object Main extends App {
     case MyNone() => "None"
     case MySome(n) => n
   })
+
+  println(ints + ".map(_ + 1) ==" + ints.map(_ + 1))
+
+  println(ints + 1.toString)
+  println(ints + ".flatMap(List(_.toDouble)) ==" + ints.flatMap(x => MyListPair(x.toDouble, MyListNil)))
 }
