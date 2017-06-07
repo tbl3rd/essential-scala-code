617211d1d9d8f09712f09257a96013167bff9ba0
Tom Lyons <tbl@broadinstitute.org>
2017-06-07 16:36:57 -0400

    Implement TimesImplicits.

--- src/main/scala/typeclass/times/Times.scala
+++ src/main/scala/typeclass/times/Times.scala
@@ -1,12 +1,14 @@
 package typeclass.times
 
 object TimesImplicits {
-  // TODO: Implement `int.times()`
+  implicit class Times(i: Int) {
+    def times[A](f: Int => A): List[A] = (1 to i).map(f).toList
+  }
 }
 
 object Main extends App {
   import TimesImplicits._
 
-  // println("""5.times(_ * 10)  == """ + 5.times(_ * 10))
-  // println("""3.times(_ + "!") == """ + 3.times(_ + "!"))
+   println("""5.times(_ * 10)  == """ + 5.times(_ * 10))
+   println("""3.times(_ + "!") == """ + 3.times(_ + "!"))
 }
